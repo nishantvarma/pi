e6f6db080b1a386ab942d43c97ac26825ae45871
Nishant Varma <nishant.varma@gmail.com>
2025-03-26 21:31:01 +0530

    Fixups

--- refs/layout.py
+++ refs/layout.py
@@ -3,29 +3,39 @@
 import tkinter as tk
 
 
-def main():
-    root = tk.Tk()
-    root.title("Editor")
-    root.option_add("*Font", ("DejaVu Sans Mono", 11))
-    main = tk.Frame(root)
-    main.pack(fill=tk.BOTH, expand=True)
-    top = tk.Text(main, height=1, bg="#d3d3d3", bd=0, relief="flat", highlightthickness=0)
-    top.pack(fill=tk.X)
-    top.insert("1.0", "/home/nishant/src/pi/pi.py | Del Cut Paste Snarf Look Undo ")
-    body = tk.Frame(main)
-    body.pack(fill=tk.BOTH, expand=True)
-    tag = tk.Text(body, height=1, bg="#ffffea", bd=1, relief="flat", highlightthickness=0)
-    tag.pack(fill=tk.X)
-    tag.insert("1.0", "/home/nishant/src/pi/pi.py | Del Cut Paste Snarf Look Undo ")
-    editor = tk.Text(body, bd=0, relief="flat", highlightthickness=0)
-    editor.pack(fill=tk.BOTH, expand=True)
-    editor.insert("1.0", "Hello, World.\n\nThanks for using Pi. Have a good day!\n")
-    bottom = tk.Text(main, height=1, bg="#d3d3d3", bd=0, relief="flat", highlightthickness=0)
-    bottom.pack(fill=tk.X)
-    bottom.insert("1.0", "1,1  All")
-    bottom.config(state="disabled")
-    root.mainloop()
+class EditorWindow(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.pack(fill=tk.BOTH, expand=True)
+        self.tag = tk.Text(self, height=1, bg="#ffffea", bd=1, relief="flat", highlightthickness=0)
+        self.tag.pack(fill=tk.X)
+        self.editor = tk.Text(self, bd=0, relief="flat", highlightthickness=0)
+        self.editor.pack(fill=tk.BOTH, expand=True)
+
+
+class MainLayout(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.pack(fill=tk.BOTH, expand=True)
+        self.top = tk.Text(self, height=1, bg="#d3d3d3", bd=0, relief="flat", highlightthickness=0)
+        self.top.pack(fill=tk.X)
+        self.body = tk.Frame(self)
+        self.body.pack(fill=tk.BOTH, expand=True)
+        self.bottom = tk.Text(self, height=1, bg="#d3d3d3", bd=0, relief="flat", highlightthickness=0)
+        self.bottom.config(state="disabled")
+        self.bottom.pack(fill=tk.X)
 
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    root.title("Editor")
+    root.option_add("*Font", ("DejaVu Sans Mono", 11))
+    layout = MainLayout(root)
+    layout.top.insert("1.0", "/home/nishant/src/pi/pi.py | Del Cut Paste Snarf Look Undo ")
+    layout.bottom.config(state="normal")
+    layout.bottom.insert("1.0", "1,1  All")
+    layout.bottom.config(state="disabled")
+    window = EditorWindow(layout.body)
+    window.tag.insert("1.0", "/home/nishant/src/pi/pi.py | Del Cut Paste Snarf Look Undo ")
+    window.editor.insert("1.0", "Hello, World.\n\nThanks for using Pi. Have a good day!\n")
+    root.mainloop()
