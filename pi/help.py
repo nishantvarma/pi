5f33185533900f307318dceaf40e4c894fdf6a7d
Nishant Varma <nishant.varma@gmail.com>
2025-12-18 15:07:31 +0530

    Fixups

--- pi/help.py
+++ pi/help.py
@@ -8,11 +8,8 @@ c = config.explorer
 
 def toggle(parent, bindings):
     global inst
-    if inst:
-        try:
-            inst.destroy()
-        except:
-            pass
+    if inst and inst.winfo_exists():
+        inst.destroy()
         inst = None
         return
     inst = Help(parent, bindings)
@@ -28,8 +25,8 @@ class Help(tk.Toplevel):
         self.create(bindings)
         self.position()
         self.deiconify()
-        self.after(100, self.bind_close)
-        self.focus_set()
+        self.bind("<Escape>", self.close)
+        self.bind("<Button-1>", self.close)
 
     def create(self, bindings):
         f = tk.Frame(self, bg=c.bg, padx=10, pady=10)
@@ -49,10 +46,6 @@ class Help(tk.Toplevel):
         x = self.parent.winfo_rootx() + (self.parent.winfo_width() - w) // 2
         y = self.parent.winfo_rooty() + (self.parent.winfo_height() - h) // 2
         self.geometry(f"{w}x{h}+{x}+{y}")
-
-    def bind_close(self):
-        self.bind("<Key>", self.close)
-        self.bind("<Button-1>", self.close)
 
     def close(self, event=None):
         global inst
